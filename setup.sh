2355e2865ea23dc7e246713b257a03b854437b1f
jotorres526 <jotorres526@gmail.com>
2020-11-23 18:23:04 +0000

    Removed symlink for kitty theme aliases gang getting changed again reordered setup.sh... still a nuke

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ currDir=pwd
 ln -s $currDir/.xmobar $HOME/.xmobar
 ln -s $currDir/.xmonad $HOME/.xmonad
 ln -s $currDir/.zsh $HOME/.zsh
-ln -s $currDir/.zshrc $HOME/.zshrc
-ln -s $currDir/compton.conf $HOME/.config/compton.conf
-ln -s $currDir/rofi $HOME/.config/rofi
 ln -s $currDir/kitty $HOME/.config/kitty
 ln -s $currDir/nvim $HOME/.config/nvim
+ln -s $currDir/rofi $HOME/.config/rofi
+ln -s $currDir/.aliases $HOME/.aliases
+ln -s $currDir/compton.conf $HOME/.config/compton.conf
+ln -s $currDir/.zshrc $HOME/.zshrc
